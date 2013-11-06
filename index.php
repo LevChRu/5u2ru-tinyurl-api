db08cef8b578eb48bd817b9134e61c06cd7fec72
Lev Chernenko <i@levch.ru>
2013-11-06 20:15:29 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$url = trim($_POST['url']);
+if(!preg_match("#^https?://#iU",$url)) $url= "http://".$url;
+
+if (!$url)
+{
+header("Location: ". $_SERVER['PHP_SELF'] . "");
+}
+else
+{
+$longurl = urlencode($url);
+}
+$request = "http://5u2.ru/API/write/get?url=$longurl";
+$json = file_get_contents($request);
+$obj = json_decode($json);
+$newurl = $obj->data->url;
+if($newurl) {
+echo $newurl;
+}
+?>
