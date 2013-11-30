b92e8ee57c353f22fba9bfec28ad98e989ad3722
Lev Chernenko <i@levch.ru>
2013-11-30 16:28:31 +0300

    Создание файла

--- json-api.php
+++ json-api.php
@@ -1,5 +1,5 @@
 <?php
-$url = trim($_POST['url']);
+$url = trim($_POST['url']); // Отправляем скрипту POST-запрос с длинной ссылкой
 if(!preg_match("#^https?://#iU",$url)) $url= "http://".$url;
 
 if (!$url) { header("Location: ". $_SERVER['PHP_SELF'] . ""); }
@@ -7,6 +7,6 @@ else { $longurl = urlencode($url); }
 $request = "http://5u2.ru/API/write/get?url=$longurl";
 $json = file_get_contents($request);
 $obj = json_decode($json);
-$newurl = $obj->data->url;
+$newurl = $obj->data->url; // Распарсиваем элемент URL, т.е. короткую ссылку
 if($newurl) { echo $newurl; }
 ?>
