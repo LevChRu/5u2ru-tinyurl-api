bf04bfdf73d17037b4b92f3199ffd183a159c259
Lev Chernenko <i@levch.ru>
2013-11-30 16:26:35 +0300

    Создание файла

--- sample-api.php
+++ sample-api.php
@@ -1,4 +1,4 @@
 <?php
-$tinyurl = file_get_contents("http://5u2.ru/api.php?url=http://example.com");
+$tinyurl = file_get_contents("http://5u2.ru/api.php?url=http://example.com"); // Создаём короткую ссылку для сайта http://example.com
 echo $tinyurl;
 ?>
