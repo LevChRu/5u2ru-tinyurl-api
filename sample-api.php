999792f468b3dc36cd30a6819cd6e413bcee8a8e
Lev Chernenko <i@levch.ru>
2013-11-30 16:41:09 +0300

    Простое API

--- sample-api.php
+++ sample-api.php
@@ -1,4 +1,9 @@
 <?php
-$tinyurl = file_get_contents("http://5u2.ru/api.php?url=http://example.com"); // Создаём короткую ссылку для сайта http://example.com
+$url = trim($_GET['url']); // Отправляем скрипту GET-запрос с длинной ссылкой (пример: sample-api.php?url=test.ru)
+if(!preg_match("#^https?://#iU",$url)) $url= "http://".$url;
+
+if (!$url) { header("Location: ". $_SERVER['PHP_SELF'] . ""); }
+else { $longurl = urlencode($url); }
+$tinyurl = file_get_contents("http://5u2.ru/api.php?url=$longurl");
 echo $tinyurl;
 ?>
